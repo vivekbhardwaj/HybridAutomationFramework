aab3710193c3e9e1c4c76f31cf87e0590cade1a8
Vivek Pathak <vivek18pathak@gmail.com>
2018-04-24 21:56:06 +0530

    Added different version of jaxb maven plugin
    Now mvn clean install will generate classes in generated directory

--- fb/src/test/java/com/fb/test/profile/Profile.java
+++ fb/src/test/java/com/fb/test/profile/Profile.java
@@ -2,13 +2,14 @@ package com.fb.test.profile;
 
 import org.testng.annotations.Test;
 
-import com.fb.test.genereated.pagedata.ObjectFactory;
-import com.fb.test.genereated.pagedata.PageData.Configuration;
+//import com.fb.test.genereated.pagedata.ObjectFactory;
+//import com.fb.test.genereated.pagedata.PageData.Configuration;
+
 
 public class Profile {
   @Test
   public void f() {
-	  Configuration conf = new ObjectFactory().createPageDataConfiguration();
-	  System.out.println("name ->"+conf.getId());
+	  //Configuration conf = new ObjectFactory().createPageDataConfiguration();
+	  //System.out.println("name ->"+conf.getId());
   }
 }

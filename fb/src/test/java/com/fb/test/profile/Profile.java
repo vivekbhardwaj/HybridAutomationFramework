988269ddd4c853072d7cc2c3a67a7ea328195ea6
Vivek Pathak <vivek18pathak@gmail.com>
2018-04-24 22:16:14 +0530

    PageData values
    Now values now coming...

--- fb/src/test/java/com/fb/test/profile/Profile.java
+++ fb/src/test/java/com/fb/test/profile/Profile.java
@@ -1,15 +1,26 @@
 package com.fb.test.profile;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.testng.annotations.Test;
 
-//import com.fb.test.genereated.pagedata.ObjectFactory;
-//import com.fb.test.genereated.pagedata.PageData.Configuration;
-
+import com.fb.test.generatedClasses.*;
+import com.fb.test.generatedClasses.PageData.Configuration; 
 
 public class Profile {
   @Test
-  public void f() {
-	  //Configuration conf = new ObjectFactory().createPageDataConfiguration();
-	  //System.out.println("name ->"+conf.getId());
+  public void f() throws JAXBException, FileNotFoundException {
+	  JAXBContext context = JAXBContext.newInstance("com.fb.test.generatedClasses");
+	  Unmarshaller unmarshler = context.createUnmarshaller();
+	  PageData data = (PageData) unmarshler.unmarshal(new FileInputStream("data/EnvironmentOne/PageData.xml"));
+	  for(Configuration conf: data.getConfiguration()){
+		  System.out.println(conf.getUsername());
+		  System.out.println(conf.getPassword());
+	  }
   }
 }
